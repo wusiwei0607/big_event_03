51d7fbeb3866339f427bd5862795ca3dbe73c415
wusiwei <120749792@qq.com>
2020-10-30 20:15:32 +0800

    个人中心功能完成

--- assets/js/user/user_avatar.js
+++ assets/js/user/user_avatar.js
@@ -12,4 +12,45 @@ $(function () {
     // 1.3 创建裁剪区域
     $image.cropper(options)
 
+    // 2. 点击上传按钮,触发文本选择框的单击事件
+    $('#btnChooseImage').on('click', function () {
+        $('#file').trigger('click')
+    })
+
+    // 3. 更换裁剪区图片
+    $('#file').on('change', function (e) {
+        var file = e.target.files[0]
+        var newImgURL = URL.createObjectURL(file)
+        $image
+            .cropper('destroy')      // 销毁旧的裁剪区域
+            .attr('src', newImgURL)  // 重新设置图片路径
+            .cropper(options)        // 重新初始化裁剪区域
+    })
+
+    // 4. 点击确定按钮,修改头像
+    var layer = layui.layer
+    $('#btnUpload').on('click', function () {
+        // 将裁剪后的图片，输出为 base64 格式的字符串
+        var dataURL = $image
+            .cropper('getCroppedCanvas', { // 创建一个 Canvas 画布
+                width: 100,
+                height: 100
+            })
+            .toDataURL('image/png')       // 将 Canvas 画布上的内容，转化为 base64 格式的字符串
+        $.ajax({
+            method: 'POST',
+            url: '/my/update/avatar',
+            data: {
+                avatar : dataURL
+            },
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('更换头像成功!')
+                }
+                layer.msg('更换头像成功!')
+                window.parent.getUserInfo()
+            }
+        })
+    })
+
 })
